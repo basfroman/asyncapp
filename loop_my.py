3a0d8feb69c13dab5955c67df2f75d5c04b33a38
basfroman <basfroman@gmail.com>
2021-12-13 02:50:27 -0800

    clean code

--- loop_my.py
+++ loop_my.py
@@ -33,9 +33,3 @@ async def start():
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
     loop.run_until_complete(start())
-
-    # loop.run_until_complete(async_print('start'))
-    # loop.run_until_complete(run())
-    # loop.run_until_complete(run())
-    # loop.run_until_complete(run())
-    # loop.run_until_complete(async_print('stop'))
